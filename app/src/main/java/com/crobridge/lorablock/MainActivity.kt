789f2de541ceccbc7e99ecc8ca7c9b8c5e7d9c66
Nenad Čaklović <nenad.caklovic@reversinglabs.com>
2023-06-24 17:36:40 +0200

    Main activity layout (using simple LinearLayout) + overflow menu

--- app/src/main/java/com/crobridge/lorablock/MainActivity.kt
+++ app/src/main/java/com/crobridge/lorablock/MainActivity.kt
@@ -2,10 +2,25 @@ package com.crobridge.lorablock
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import com.crobridge.lorablock.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.appToolbar.setTitle(R.string.app_name)
+        setSupportActionBar(binding.appToolbar)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.overflow_menu, menu)
+        return true
+    }
+
 }
